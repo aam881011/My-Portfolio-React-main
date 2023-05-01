e380a82af53388aa426d17552503ddaea0684676
Mohamed Dahy <Alazhry@gmail.com>
2023-05-01 19:06:42 +0200

    change nav link

--- src/component/ContactUs/ContactUs.jsx
+++ src/component/ContactUs/ContactUs.jsx
@@ -1,7 +1,7 @@
 import "./ContactUs.scss";
 // import CustomerReviews from "../CustomerReviews/CustomerReviews";
-import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
 
 const ContactUs = () => {
 
